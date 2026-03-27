a694a7a13f28d6972b4f8d95e26d2f3a9d0a3ab0
mango-habanero <main@mango-habanero.dev>
2026-03-27 09:10:00 +0300

    refactor(ops): remove unused environment variables and dependencies

--- apps/ops/src/lib/env.ts
+++ apps/ops/src/lib/env.ts
@@ -1,12 +1,4 @@
-import { getRequiredEnv } from "@repo/shared/server";
-
 export const environment = process.env.NODE_ENV as
   | "development"
   | "production"
   | "test";
-
-export const nextAuthSecret = getRequiredEnv("NEXTAUTH_SECRET");
-export const whitelistedEmailDomains = getRequiredEnv(
-  "WHITELISTED_EMAIL_DOMAINS",
-  "",
-).split(",");
