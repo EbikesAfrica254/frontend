7070b4afa350bb5147397b52492dcddab9438d63
mango-habanero <main@mango-habanero.dev>
2026-03-27 09:39:58 +0300

    style(auth): normalize indentation and spacing in keycloak and next-auth types

--- packages/features/auth/src/types/next-auth.d.ts
+++ packages/features/auth/src/types/next-auth.d.ts
@@ -1,18 +1,17 @@
 import "next-auth";
 import "next-auth/jwt";
-import type {AuthToken, AuthUser} from "./keycloak";
+import type { AuthToken, AuthUser } from "./keycloak";
 
 declare module "next-auth" {
-    interface Session {
-        accessToken: string;
-        aud?: string | string[];
-        error?: "RefreshAccessTokenError";
-        user: AuthUser;
-    }
+  interface Session {
+    accessToken: string;
+    aud?: string | string[];
+    error?: "RefreshAccessTokenError";
+    user: AuthUser;
+  }
 }
 
 declare module "next-auth/jwt" {
-    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-    interface JWT extends AuthToken {
-    }
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  interface JWT extends AuthToken {}
 }
