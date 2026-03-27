7c2a2b1b8971793d7107877b53ae7636356ac34c
mango-habanero <main@mango-habanero.dev>
2026-03-27 09:37:42 +0300

    refactor(client): remove unused nextauth secret from env configuration

--- apps/client/src/lib/env.ts
+++ apps/client/src/lib/env.ts
@@ -1,8 +1,4 @@
-import { getRequiredEnv } from "@repo/shared/server";
-
 export const environment = process.env.NODE_ENV as
   | "development"
   | "production"
   | "test";
-
-export const nextAuthSecret = getRequiredEnv("NEXTAUTH_SECRET");
